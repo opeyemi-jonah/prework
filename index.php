69eccd443141ecb84f885dc00f83856fea04ae8b
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-02-14 11:58:31 -0700

    I wrote an html code  for the index starter page.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+	<meta charset="utf-8"/>
+	<title>I completed the Prework!</title>
+
+	<body>
+
+<p>
+	I've completed the prework!!!
+</p>
+</body>
+</head>
+
+
+
+
+</html>
